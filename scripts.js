463546a2b8e0028c9ffb706ef5c7c4cce5f66080
cod3r12 <85259706+cod3r12@users.noreply.github.com>
2025-03-16 14:22:42 -0500

    Restructure database

--- scripts.js
+++ scripts.js
@@ -1,29 +1,27 @@
 function setupStacks () {
-    // localStorage.clear();
-    if (localStorage.getItem("numStacks") === null) {
-        localStorage.clear();
-        localStorage.setItem("numStacks", 0);
+    // localStorage.removeItem("liamb09-flashcards");
+    if (localStorage.getItem("liamb09-flashcards") === null) {
+        localStorage.setItem("liamb09-flashcards", JSON.stringify([0, [], []])); // numStacks, stackNames, stackData
     }
 
-    if (localStorage.getItem("numStacks") == 0) {
+    if (JSON.parse(localStorage.getItem("liamb09-flashcards"))[0] == 0) {
         document.getElementById("no-stacks-message").innerHTML = "<br /><br />You have no stacks saved.";
+        return;
     } else {
         document.getElementById("no-stacks-message").innerHTML = "";
     }
 
-    numStacks = parseInt(localStorage.getItem("numStacks"));
-    console.log(numStacks);
-    for (i = 1; i <= numStacks; i++) {
-        stack = JSON.parse(localStorage.getItem(`stack${i}`));
-        console.log(stack);
-        document.getElementById("stack-list").innerHTML += getStack(stack, i);
+    stackNames = JSON.parse(localStorage.getItem("liamb09-flashcards"))[1];
+    stackData = JSON.parse(localStorage.getItem("liamb09-flashcards"))[2];
+    for (i = 0; i < stackNames.length; i++) {
+        document.getElementById("stack-list").innerHTML += getStack(stackNames[i], stackData[i], i);
     }
 }
 
-function getStack (stack) {
-    return `<div id="stack-card">
-                <h1>${stack[0]}</h1>
-                <p>${stack[1].length} terms</p>
+function getStack (stackName, stackData, id) {
+    return `<div class="stack-card" id="stackCard${id}" onclick="viewStack(this.id)">
+                <h1>${stackName}</h1>
+                <p>${stackData.length} terms</p>
             </div>`;
 }
 
@@ -69,19 +67,40 @@ function getCard (cardData, index) {
 }
 
 function saveStack () {
+    // Make sure user has inputted title
     if (document.getElementById("title-input").value == "") {
         document.getElementById("error-message").innerHTML = "Please enter a title for your stack.";
         return;
     }
-    console.log(stack);
+
+    // Make sure user has chosen unique stack name
+    stackNames = JSON.parse(localStorage.getItem("liamb09-flashcards"))[1];
+    if (stackNames.includes(document.getElementById("title-input").value)) {
+        document.getElementById("error-message").innerHTML = "You already have a stack with that name. Please choose a different name.";
+        return;
+    }
+
+    // Make sure all fields are filled
+    var allFieldsFilled = true;
     stack.forEach((card) => {
         if (card[0] == "" || card[1] == "") {
             document.getElementById("error-message").innerHTML = "Please fill out all fields.";
+            allFieldsFilled = false;
             return;
         }
     });
-    localStorage.setItem("numStacks", parseInt(localStorage.getItem("numStacks")) + 1);
-    jsonStack = JSON.stringify([document.getElementById("title-input").value, stack]);
-    console.log(stack);
-    localStorage.setItem(`stack${localStorage.getItem("numStacks")}`, jsonStack);
+    if (!allFieldsFilled) return;
+
+    // Save stack
+    currentStackData = JSON.parse(localStorage.getItem("liamb09-flashcards"));
+    currentStackData[0] = parseInt(currentStackData[0]) + 1
+    currentStackData[1].unshift(document.getElementById("title-input").value);
+    currentStackData[2].unshift(stack);
+    localStorage.setItem("liamb09-flashcards", JSON.stringify(currentStackData));
+    window.location.href = 'index.html';
+}
+
+function viewStack (id) {
+    var idNum = id.match(/\d+/g)[0];
+    window.location.href = `viewer.html?setID=${idNum}`;
 }
